56c0b90f96081dbb05c44d5c3e6f4dd7fed09c09
networkSocerer <gigattfyhig@gmail.com>
2025-07-15 18:24:46 +0900

    insurance

--- src/main/Gemini/app.py
+++ src/main/Gemini/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.pdf_routes import pdf_bp
 
 app = Flask(__name__)
 app.register_blueprint(pdf_bp)
+
 @app.route("/")
 def index():
-    return "✅ Flask 서버가 정상적으로 작동 중입니다!"
+    return render_template("upload.html")  # templates/upload.html 불러오기
 
 if __name__ == "__main__":
     app.run(debug=True)
